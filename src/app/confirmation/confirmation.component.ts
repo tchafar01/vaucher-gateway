1d471cb3731ac20263c4594c1d0b55bb0c2525de
MAROUANE SABER <sabermarwan2@gmail.com>
2023-04-12 11:56:30 +0000

    fix

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -7,15 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ConfirmationComponent {
 
-  cho "# vaucher-gateway" >> README.md
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/tchafar01/vaucher-gateway.git
-  git push -u origin main
-
-
-
 
 }
